c5dd2844c0204a4b2d390163634917243445f555
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-07 01:59:55 +0600

    create mongoose covvector

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//database connection
+const connecDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGOOSE_CONNECTION_URL)
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Could not connect to MongoDB', err);
+        process.exit(1);
+    }
+}
+
+
+app.listen(port, () => {
+    connecDB();
+    console.log(`Server is running on port ${port}`);
+});
